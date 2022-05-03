eae9f2e975185238b88158e42dc0160e7d73528d
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-05-04 00:34:54 +0200

    ready

--- results/02_run.sh
+++ results/02_run.sh
@@ -6,7 +6,13 @@ chmod 777 toppodm.sh
 chmod 777 vmdool.sh
 chmod 777 text.sh
 chmod 777 cross.sh
+
+./toppodd.sh &
+./toppodkf.sh &
+./toppodks.sh &
+./vmdool.sh &
 ./topnode.sh &
 ./toppodm.sh &
 ./cross.sh &
+./text.sh
 python3 scrapetime.py &
--- results/03_cptorennes.sh
+++ results/03_cptorennes.sh
@@ -6,5 +6,5 @@ mv kubetopPodKF.csv /root/mck8s_vm/results/results/kubetopPodKF.csv
 mv psr.csv /root/mck8s_vm/results/results/psr.csv
 mv plaintext /root/mck8s_vm/results/results/plaintext
 mv cross /root/mck8s_vm/results/results/cross
-
+mv prom_scrape_rntsm /root/mck8s_vm/results/results/prom_scrape_rntsm
 scp -r /root/mck8s_vm/results/results chuang@172.16.111.106:/home/chuang/results
