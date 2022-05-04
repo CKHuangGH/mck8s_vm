5846dcf5ad6aba0bbebe124575656ecb785f472b
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-05-05 00:28:12 +0200

    Update 03_getdocker.sh

--- results/03_getdocker.sh
+++ results/03_getdocker.sh
@@ -1,4 +1,4 @@
 ssh -o StrictHostKeyChecking=no root@10.158.4.2 chmod 777 /root/mck8s_vm/results/dockergetime.sh
-ssh -o StrictHostKeyChecking=no root@10.158.4.2 ./root/mck8s_vm/results/dockergetime.sh
+ssh -o StrictHostKeyChecking=no root@10.158.4.2 . /root/mck8s_vm/results/dockergetime.sh
 ssh -o StrictHostKeyChecking=no root@10.158.0.3 chmod 777 /root/mck8s_vm/results/dockergettimemanage.sh
-ssh -o StrictHostKeyChecking=no root@10.158.0.3 ./root/mck8s_vm/results/dockergettimemanage.sh
+ssh -o StrictHostKeyChecking=no root@10.158.0.3 . /root/mck8s_vm/results/dockergettimemanage.sh
