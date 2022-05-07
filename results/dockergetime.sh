1e72f9de044ffa653bad6b5dce9f56f4e6dd6ba5
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-05-07 23:16:47 +0200

    test

--- results/dockergetime.sh
+++ results/dockergetime.sh
@@ -3,4 +3,3 @@ for i in $(cat name)
 do
  docker cp $i:/exectime /root/
 done
-scp /root/exectime root@10.158.0.2:/root/mck8s_vm/results/results/exectime
--- results/dockergettimemanage.sh
+++ results/dockergettimemanage.sh
@@ -3,4 +3,3 @@ for i in $(cat name)
 do
  docker cp $i:/exectime_management /root/
 done
-scp /root/exectime_management root@10.158.0.2:/root/mck8s_vm/results/results/exectime_management
