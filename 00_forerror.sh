d27f5e7c84d5d6720bcc6bb6119b812410e8d5d0
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-08-22 00:18:58 +0200

    g

---
+++ 00_forerror.sh
@@ -0,0 +1,4 @@
+. /01_combineAll.sh
+. /04_mck8s-2.sh
+. /05_joining_test.sh
+. /06_deployment.sh
--- error/02_run.sh
+++ error/02_run.sh
@@ -1,3 +1,4 @@
+kubectl apply -f /root/acala_error/member/deploy_member.yaml
 python3 /root/k8s_google_injection/run_deployments_jobs_mck8s.py
 sleep(1800)
 ./03_getdocker.sh
