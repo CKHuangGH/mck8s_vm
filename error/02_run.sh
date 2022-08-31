61c723b1ae936b060bae3f01383622169acfd145
CKHuangGH <chihkaihuang1@gmail.com>
2022-08-31 11:05:42 +0200

    Update 02_run.sh

--- error/02_run.sh
+++ error/02_run.sh
@@ -1,5 +1,5 @@
-ssh -o StrictHostKeyChecking=no root@10.158.4.2 kubectl apply -f /root/acala_error/member/deploy_member.yaml
+ssh -o StrictHostKeyChecking=no root@10.158.4.2 kubectl apply -f /root/acala_error/02.member_stander/deploy_member.yaml
 sleep 30
 python3 /root/k8s_google_injection/run_deployments_jobs_mck8s.py
-sleep 900
+sleep 1800
 ./03_getdocker.sh
