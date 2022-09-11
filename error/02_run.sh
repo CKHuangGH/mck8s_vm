fdf9e5563266241a0e0a6d74de25699e77d0e235
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-09-11 17:57:09 +0200

    Update 02_run.sh

--- error/02_run.sh
+++ error/02_run.sh
@@ -1,3 +1,5 @@
+ssh -o StrictHostKeyChecking=no root@10.158.4.2 docker pull chuangtw/acalamember:latest
+sleep 10
 ssh -o StrictHostKeyChecking=no root@10.158.4.2 kubectl apply -f /root/acala_error/02.member_stander/deploy_member.yaml
 sleep 60
 python3 /root/k8s_google_injection/run_deployments_jobs_mck8s.py
