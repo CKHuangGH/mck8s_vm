94247964d1493006120ffac330d1a83fa4a1d61a
CKHuangGH <chihkaihuang1@gmail.com>
2022-09-28 15:26:08 +0200

    Update 00_formcdeploy.sh

--- mcdeploy/00_formcdeploy.sh
+++ mcdeploy/00_formcdeploy.sh
@@ -6,8 +6,8 @@ sleep 10
 sleep 10
 ./05_joining_test.sh
 sleep 10
-./06_deployment.sh
-sleep 100
+#./06_deployment.sh
+#sleep 100
 # chmod 777 /root/mck8s_vm/error/01_status.sh
 # chmod 777 /root/mck8s_vm/error/02_run.sh
 # chmod 777 /root/mck8s_vm/error/03_getdocker.sh
