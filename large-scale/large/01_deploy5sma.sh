1ccd6e218f66bdfdb36da9476a7c4eb7bacb05b0
CKHuangGH <chihkaihuang1@gmail.com>
2022-10-11 12:20:49 +0200

    ready

---
+++ large-scale/large/01_deploy5sma.sh
@@ -0,0 +1,18 @@
+cp ../node_list node_list
+cp ../node_list_all node_list_all
+j=1
+for i in $(cat node_list)
+do
+	echo "deploy member at $i"
+	##change here
+	ssh root@$i kubectl apply -f /root/mck8s_vm/large-scale/acala/member/ma/deploy_member.yaml  
+	j=$((j+1))	
+done
+echo "wait 30s-------------------------------"
+sleep 30
+##change here
+kubectl apply -f /root/mck8s_vm/large-scale/acala/controller/5s/deploy_controller.yaml
+echo "wait 60s-------------------------------"
+sleep 60
+
+echo "Good for check"
