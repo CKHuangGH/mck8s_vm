5ba71e3be10c46d70d245b8c2bf9cbdc3f550d88
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-02-25 20:37:59 +0100

    node

--- sasi/5nodes/promvalue60s.sh
+++ sasi/5nodes/promvalue60s.sh
@@ -7,7 +7,7 @@ do
 	ip2=$(echo $i | cut -d "." -f 3)
     name=cluster$j
 	echo "      - job_name: '$name' " >> values.yaml
-	echo "        scrape_interval: 5s" >> values.yaml
+	echo "        scrape_interval: 60s" >> values.yaml
 	echo "        metrics_path: /federate" >> values.yaml
 	echo "        honor_labels: true" >> values.yaml
 	echo "        scheme: http" >> values.yaml
@@ -18,11 +18,6 @@ do
 	echo "            - '{instance=~\"10.$ip1.$ip2.4:9100\"}'" >> values.yaml
 	echo "            - '{instance=~\"10.$ip1.$ip2.5:9100\"}'" >> values.yaml
 	echo "            - '{instance=~\"10.$ip1.$ip2.6:9100\"}'" >> values.yaml
-	echo "            - '{instance=~\"10.$ip1.$ip2.7:9100\"}'" >> values.yaml
-	echo "            - '{instance=~\"10.$ip1.$ip2.8:9100\"}'" >> values.yaml
-	echo "            - '{instance=~\"10.$ip1.$ip2.9:9100\"}'" >> values.yaml
-	echo "            - '{instance=~\"10.$ip1.$ip2.10:9100\"}'" >> values.yaml
-	echo "            - '{instance=~\"10.$ip1.$ip2.11:9100\"}'" >> values.yaml
 	echo "        tls_config:" >> values.yaml	
 	echo "          insecure_skip_verify: true" >> values.yaml		
 	echo "        static_configs:" >> values.yaml	
