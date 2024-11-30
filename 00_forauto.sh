0015835bde0b365122568168fcc58002fecb524e
CKHuang-public <95505429+CKHuangGH@users.noreply.github.com>
2024-11-30 23:25:13 +0100

    Create 00_forauto.sh

---
+++ 00_forauto.sh
@@ -0,0 +1,14 @@
+./01_combineAll.sh
+sleep 10
+./02_acalavalue.sh
+sleep 10
+./03_createmember.sh
+sleep 10
+./04_mck8s-2.sh
+sleep 10
+./05_joining_test.sh
+sleep 10
+./06_deployment.sh
+sleep 100
+
+echo "good to start run error"
